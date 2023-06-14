505d65a829ac2aa07e8e382c0ef36c237c41044b
Kinjal Jain <kinjal@wolkensoftware.com>
2023-06-14 12:43:05 +0530

    Button component added

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import { useState} from 'react';
 import { createAuthUserWithEmailAndPassword ,createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+import './sign-up-form.styles.scss';
+import Button from '../button/button.component';
 
 import FormInput from '../form-input/form-input.component';
 
@@ -53,8 +55,9 @@ const SignUpForm = () =>{
     }
 
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                <FormInput 
                     label="Display Name"
@@ -78,7 +81,7 @@ const SignUpForm = () =>{
             
                 <FormInput label="Confirm Password" inputOptions={{type:"password", required: true, onChange:handleChange ,name:"confirmPassword" ,value:confirmPassword}} />
 
-                <button type="submit">Sign up</button>
+                <Button type="submit"  buttonType='inverted' children='SIGN UP'/>
             </form>
         </div>
     )
