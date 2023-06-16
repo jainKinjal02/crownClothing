2257b7e31d258939390ebd662e9c77414cd45476
Kinjal Jain <kinjal@wolkensoftware.com>
2023-06-16 15:53:30 +0530

    Checkout component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+
 const App = ()=> {
  return (
   <Routes>
@@ -10,6 +12,7 @@ const App = ()=> {
       <Route index element={<Home />}/> {/* matching path is /home */}
       <Route path='shop' element={<Shop />}/> {/* matching path is /shop */}
       <Route path='auth' element={<Authentication />}/>
+      <Route path='checkout' element={<Checkout />}/>
     </Route>
   </Routes>
  );
