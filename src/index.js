45fc938350e92ef06ddfe68753466e75cd25cc70
Kinjal Jain <kinjal@wolkensoftware.com>
2023-06-29 11:28:09 +0530

    categories reducer added from categories context migrated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 
-import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context';
 import { store } from './store/store';
 import reportWebVitals from './reportWebVitals';
@@ -17,11 +16,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
     <BrowserRouter>
-      <CategoriesProvider>
         <CartProvider>
           <App />
         </CartProvider>
-      </CategoriesProvider>
     </BrowserRouter>
     </Provider>
   </React.StrictMode>
