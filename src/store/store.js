9b43a81e22b93f8bf334283f6b801ec68211c19c
Kinjal Jain <kinjal@wolkensoftware.com>
2023-07-05 16:14:28 +0530

    Redux-persist added

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { compose , createStore, applyMiddleware} from 'redux';
 //import logger from 'redux-logger';
-
+import {persistStore , persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {rootReducer} from './root-reducer';
 // root-reducer
 const loggerMiddleware = (store) => (next) => (action) => {
@@ -16,8 +17,19 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('next state: ', store.getState());
 
 }
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer , undefined, composedEnhancers);
+export const store = createStore(persistedReducer , undefined, composedEnhancers); //store will be using persistedreducer that is the reducer persisting all latest changes as per local storage changes
+
+export const persistor = persistStore(store);
