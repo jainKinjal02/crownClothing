d403f77e02488d8dee0d3e61caee9df29e4f0ad3
Kinjal Jain <jainkinjal021196@gmail.com>
2023-07-19 15:58:02 +0530

    Saga added to shop and category

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -3,7 +3,6 @@ import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 import { createAction } from "../../utils/reducers/reducer.utils";
 
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const setCategories = (categoriesArray) => createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
 
@@ -16,13 +15,3 @@ createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS , categoriesArray)
 export const fetchCategoriesFailed = (error)=>
 createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED , error);
 
-export const fetchCategoriesAsync = () =>async (dispatch)=>{
-    dispatch(fetchCategoriesStart());
-    try{ 
-        const categoriesArray = await getCategoriesAndDocuments('categories');
-        dispatch(fetchCategoriesSuccess(categoriesArray));
-    }catch(error){
-        dispatch(fetchCategoriesFailed(error));
-    }
-
-}
--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,5 +1,6 @@
 import {all, call } from 'redux-saga/effects';
+import { categoriesSaga } from './categories/categories.saga';
 
 export function* rootSaga(){ // generator function *
-
+    yield all([call(categoriesSaga)]);
 }
